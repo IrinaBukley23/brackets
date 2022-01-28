7be63042763985194260cc3bd12a83a6567041f3
Irina <irenserge@mail.ru>
2022-01-28 19:05:37 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let res = bracketsConfig.join(',').split(',').join('');
+  console.log(res);
+  let brackets = [];
+  for (let i = 0; i < res.length; i += 2) {
+    brackets.push(res.slice(i, i + 2));
+  }
+  console.log(brackets);
+  let result = true;
+  let restLength = str.length;
+  while (result) {
+    restLength = str.length;
+    for (let i = 0; i < brackets.length; i++) {
+      str = str.replace(brackets[i],'');
+    }
+    if (restLength === str.length) {
+      result = false;
+    }
+  }
+  return (str.length === 0);
 }
